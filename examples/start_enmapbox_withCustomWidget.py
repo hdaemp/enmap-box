3ae30c69280deb50404ef1ce6968dc74b47cb2d8
Andreas Janz <45030715+janzandr@users.noreply.github.com>
2022-09-09 12:43:18 +0200

    Update start_enmapbox_withCustomWidget.py

--- examples/start_enmapbox_withCustomWidget.py
+++ examples/start_enmapbox_withCustomWidget.py
@@ -1,8 +1,8 @@
 from enmapbox import EnMAPBox
 
 from enmapbox.testing import start_app
-from qgis._core import QgsProcessingParameterDefinition, QgsProcessingParameterFile, QgsProcessingContext
-from qgis._gui import QgsProcessingAbstractParameterDefinitionWidget, QgsAbstractProcessingParameterWidgetWrapper, \
+from qgis.core import QgsProcessingParameterDefinition, QgsProcessingParameterFile, QgsProcessingContext
+from qgis.gui import QgsProcessingAbstractParameterDefinitionWidget, QgsAbstractProcessingParameterWidgetWrapper, \
     QgsProcessingParameterWidgetContext, QgsGui
 
 qgsApp = start_app()
@@ -15,13 +15,16 @@ class MyParameterDefinitionWidget(QgsProcessingAbstractParameterDefinitionWidget
 
 class MyParameterWidgetWrapper(QgsAbstractProcessingParameterWidgetWrapper):
 
+    def __init__(self, *args, **kwargs):
+        QgsAbstractProcessingParameterWidgetWrapper.__init__(self, *args, **kwargs)
+
     def createParameterDefinitionWidget(
             self, context: QgsProcessingContext, widgetContext: QgsProcessingParameterWidgetContext
     ):
         return MyParameterDefinitionWidget(context, widgetContext)
 
-QgsGui.processingGuiRegistry().addParameterWidgetFactory(MyParameterWidgetWrapper())
-
+myParameterWidgetWrapper = MyParameterWidgetWrapper()
+QgsGui.processingGuiRegistry().addParameterWidgetFactory(myParameterWidgetWrapper)
 
 enmapBox = EnMAPBox()
 qgsApp.exec_()
